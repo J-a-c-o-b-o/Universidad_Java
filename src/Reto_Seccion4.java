9b890c609b21573bde6e54501db90b6bda28c6c6
J-a-c-o-b-o <correojacobo@gmail.com>
2024-12-29 12:38:31 +0100

    Reto S4

--- src/Reto_Seccion4.java
+++ src/Reto_Seccion4.java
@@ -1,15 +1,19 @@
 public class Reto_Seccion4 {
-    //Generador de Email a partir de los siguientes datos:
-    var nombreUsuario = "Jacobo Jim Pul";
-    var nombreEmpresa = "Global Mentoring";
-    var extensionDominio = ".com.es";
+    public static void main(String[] args) {
+        //Generador de Email a partir de los siguientes datos:
+        var nombreUsuario = "Jacobo Jim Pul";
+        var nombreEmpresa = "Global Mentoring";
+        var extensionDominio = ".com.es";
 
-    //Tengo que usar estos datos:
-    var usuarioNormalizado = "jacobo.jim.pul";
-    var dominioEmailNormalizado = "@.globalmentoring.com.es";
+        //Tengo que usar estos datos:
+        var usuarioNormalizado = "jacobo.jim.pul";
+        var dominioEmailNormalizado = "@.globalmentoring.com.es";
 
-    //Para generar este resultado final:
-    var emailFinalGenerado = "jacobo.jim.pul.@globalmentoring.com.es";
+        //Para generar este resultado final:
+        var emailFinalGenerado = "jacobo.jim.pul.@globalmentoring.com.es";
+
+        System.out.println(emailFinalGenerado);
+    }
 
 
 }
