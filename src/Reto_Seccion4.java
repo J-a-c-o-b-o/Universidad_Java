3d849b60ea3e77b0b297893c324f1f6bc66701c4
J-a-c-o-b-o <correojacobo@gmail.com>
2024-12-29 13:12:30 +0100

    Reto S4 finalizado

--- src/Reto_Seccion4.java
+++ src/Reto_Seccion4.java
@@ -5,14 +5,28 @@ public class Reto_Seccion4 {
         var nombreEmpresa = "Global Mentoring";
         var extensionDominio = ".com.es";
 
-        //Tengo que usar estos datos:
-        var usuarioNormalizado = "jacobo.jim.pul";
-        var dominioEmailNormalizado = "@.globalmentoring.com.es";
+        /**Tengo que transformar los anteriores datos para que me genere lo siguiente: Usuario Normalizado = "jacobo.jim.pul" Dominio Email Normalizado = "@.globalmentoring.com.es"**/
 
-        //Para generar este resultado final:
-        var emailFinalGenerado = "jacobo.jim.pul.@globalmentoring.com.es";
+        //Para generar este resultado final: "jacobo.jim.pul.@globalmentoring.com.es";
 
-        System.out.println(emailFinalGenerado);
+        //Empezar:
+        var reemplazaEspacioUsuario = nombreUsuario.replace(' ', '.');
+        System.out.println("reemplazaEspacioUsuario: " + reemplazaEspacioUsuario);
+
+        var minusculasSinEspaciosUsuario = reemplazaEspacioUsuario.toLowerCase();
+        System.out.println("minusculasSinEspaciosUsuario: " + minusculasSinEspaciosUsuario);
+
+        var minusculasEmpresa = nombreEmpresa.toLowerCase();
+        System.out.println("minusculasEmpresa: " + minusculasEmpresa);
+
+        var subcadena1 = minusculasEmpresa.substring(0, 6);
+        var subcadena2 = minusculasEmpresa.substring(7, 16);
+        var minusculasSinEspaciosEmpresa = subcadena1 + subcadena2;
+        System.out.println("subcadenas unidas: " + subcadena1+subcadena2);
+
+        //Uno todo para el resultado final
+        System.out.println("resultado final: " + minusculasSinEspaciosUsuario + '@' + minusculasSinEspaciosEmpresa + extensionDominio);
+
     }
 
 
