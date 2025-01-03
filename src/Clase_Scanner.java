10f77744e0036f12c932d2e603c471445662c1d9
j-a-c-o-b-o <correojacobo@gmail.com>
2025-01-03 14:09:32 +0100

    seccion 5

---
+++ src/Clase_Scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Clase_Scanner {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Introduce una línea de texto");
+        var linea = scanner.nextLine();
+        System.out.println("La línea introducida es: " + linea);
+    }
+}
---
+++ src/Reto_Seccion5.java
@@ -0,0 +1,11 @@
+public class Reto_Seccion5 {
+    /**El programa debe solicitar al usuario:
+     * -Nombre: Jacobo -> JA
+     * - Apellidos: Jimenez -> JI
+     * - Año nacimiento: 1987 -> 87
+     * - Debe generar un número aleatorio de 4 dígitos, si es menor deberá completarse con ceros a la izquierda. P.ej: 1234
+     * - ID Unico: JAJI871234
+     * **/
+
+
+}
