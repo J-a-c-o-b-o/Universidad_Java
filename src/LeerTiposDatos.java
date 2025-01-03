9755692d7044916e22eca20409d35abfcce190c6
j-a-c-o-b-o <correojacobo@gmail.com>
2025-01-03 19:56:57 +0100

    reto 1 seccion 5 finalizado

--- src/LeerTiposDatos.java
+++ src/LeerTiposDatos.java
@@ -8,5 +8,17 @@ public class LeerTiposDatos {
         System.out.print("Ingresa tu edad: ");
         var edad = consola.nextInt();
         System.out.println("Edad: " + edad);
+
+        //Leer un tipo double
+        System.out.println("Ingresa tu altura: ");
+        var altura = consola.nextDouble();
+        System.out.println("altura: " + altura);
+        //Consumir el caracter de salto de linea
+        consola.nextLine();
+
+        //Leer un tipo String
+        System.out.println("Ingresa tu nombre: ");
+        var nombre = consola.nextLine();
+        System.out.println("nombre = " + nombre);
     }
 }
--- src/Reto_Seccion5.java
+++ src/Reto_Seccion5.java
@@ -1,11 +1,43 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Reto_Seccion5 {
     /**El programa debe solicitar al usuario:
      * -Nombre: Jacobo -> JA
-     * - Apellidos: Jimenez -> JI
+     * - Apellido: Jimenez -> JI
      * - Año nacimiento: 1987 -> 87
      * - Debe generar un número aleatorio de 4 dígitos, si es menor deberá completarse con ceros a la izquierda. P.ej: 1234
      * - ID Unico: JAJI871234
      * **/
+    public static void main(String[] args) {
+        //Nombre
+        var consola = new Scanner(System.in);
+        System.out.println("Ingresa tu nombre: ");
+        var nombre = consola.nextLine();
+        var subcadenaNombre = nombre.substring(0, 2).toUpperCase();
+        System.out.println("Subcadena nombre: " + subcadenaNombre);
 
+        //Apellido
+        System.out.println("Ingresa tu apellido: ");
+        var apellido = consola.nextLine();
+        var subcadenaApellido = apellido.substring(0, 2).toUpperCase();
+        System.out.println("Subcadena apellido: " + subcadenaApellido);
+
+        //Año
+        System.out.println("Ingresa el año de nacimiento: ");
+        var anio = consola.nextInt();
+        var toStringAnio = Integer.toString(anio);
+        var subcadenaAnio = toStringAnio.substring(2, 4);
+        System.out.println("Subcadena año: " + subcadenaAnio);
+
+        //Numero aleatorio
+        Random random = new Random();
+        var numeroAleatorio = random.nextInt(0000, 10000);
+        System.out.println("Numero aleatorio generado: " + numeroAleatorio);
+
+        //ID Unico
+        var iD = subcadenaNombre + subcadenaApellido + subcadenaAnio + numeroAleatorio;
+        System.out.println("El id único es: " + iD);
+    }
 
 }
