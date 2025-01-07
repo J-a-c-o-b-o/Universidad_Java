40b39ee827703d93a410855d0886f6209f26b7a2
J-a-c-o-b-o <correojacobo@gmail.com>
2025-01-07 12:30:11 +0100

    PrestamoLibros

---
+++ src/PrestamoLibros.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PrestamoLibros {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        var console = new Scanner(System.in); 
+        var user = console.nextLine();
+        System.out.println("user = " + user);
+        System.out.println("How many kilometres do you live from here?");
+        var kms = console.nextLine();
+        System.out.println("kms = " + kms);
+        var kmsToInt = Integer.parseInt(kms);
+        var noMoreThan3kms = kmsToInt <= 3?true:false;
+        System.out.println("noMoreThan3kms = " + noMoreThan3kms);
+
+    }
+}
