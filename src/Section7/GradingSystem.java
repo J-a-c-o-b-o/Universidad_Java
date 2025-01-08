1f7d1e0ace125ecfb17af470b5ad625cfa7d7d9a
J-a-c-o-b-o <correojacobo@gmail.com>
2025-01-08 08:07:40 +0100

    grading system

---
+++ src/Section7/GradingSystem.java
@@ -0,0 +1,14 @@
+package Section7;
+
+import java.util.Scanner;
+
+public class GradingSystem {
+    public static void main(String[] args) {
+        System.out.println("What grade did you get (from 0 to 10)?");
+        var console = new Scanner(System.in);
+        var grade = Integer.parseInt(console.nextLine());
+        //System.out.println("grade = " + grade);
+
+
+    }
+}
