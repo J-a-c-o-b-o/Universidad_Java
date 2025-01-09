fe23e32a7ea861671112150ffda706a6993122cd
J-a-c-o-b-o <correojacobo@gmail.com>
2025-01-09 11:16:53 +0100

    GradingSystem

--- src/Section7/GradingSystem.java
+++ src/Section7/GradingSystem.java
@@ -7,8 +7,21 @@ public class GradingSystem {
         System.out.println("What grade did you get (from 0 to 10)?");
         var console = new Scanner(System.in);
         var grade = Integer.parseInt(console.nextLine());
+        String letterGrade;
         //System.out.println("grade = " + grade);
 
+        if(grade >= 9){
+            letterGrade = "A";
+        } else if (grade >=8 && grade <9) {
+            letterGrade = "B";
+        } else if (grade >= 7 && grade < 8) {
+            letterGrade = "C";
+        } else if (grade <= 6){
+            letterGrade = "D";
+        }else{
+            letterGrade = "You have entered incorrect punctuation";
+        }
 
+        System.out.println("Your grade is = " + letterGrade);
     }
 }
