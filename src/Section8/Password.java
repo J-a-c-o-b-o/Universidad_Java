4fc852e2b3c6c4a48fa361a051a30492c2738b17
j-a-c-o-b-o <correojacobo@gmail.com>
2025-01-17 00:06:22 +0100

    Password

--- src/Section8/Password.java
+++ src/Section8/Password.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 
 public class Password {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Introduce a password (must be 6 characters): ");
-        var password = console.nextLine();
-        System.out.println("password = " + password);
-        int characters = 0;
-        int minCharacters = 6;
-        
-        while(characters < 6){
-            System.out.println("The minimun of characters is 6. You've putted: " + characters);
+        String password;
+
+        do{
+            Scanner console = new Scanner(System.in);
+            System.out.println("Introduce a password (must be 6 characters): ");
+            password = console.nextLine();
+            System.out.println("password = " + password);
+            if(!isValidPassword(password)){
+                System.out.println("The password is NOT correct (it has the length of: " + password.length() + " )");
+            }
+
+        }while(!isValidPassword(password));
+
+        if(isValidPassword(password)) {
+            System.out.println("The password is correct (it has the length of: " + password.length() + " )");
         }
-        
+    }
+    public static boolean isValidPassword(String password){
+        return password.length()>=6;
     }
 }
