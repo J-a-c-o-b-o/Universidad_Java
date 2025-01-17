4d32c832ac4b28f9c22ea3cfe8e1cccca52dc1ab
j-a-c-o-b-o <correojacobo@gmail.com>
2025-01-17 20:10:34 +0100

    GuessingGame

--- src/Section8/GuessingGame.java
+++ src/Section8/GuessingGame.java
@@ -5,13 +5,38 @@ import java.util.Scanner;
 
 public class GuessingGame {
     public static void main(String[] args) {
-        System.out.println("Do you have to guess a secret number. So put a number: ");
-        Scanner console = new Scanner(System.in);
-        int number = Integer.parseInt(console.nextLine());
+
+
         Random shuffle = new Random();
         int shuffleNumber = shuffle.nextInt(50)+1;
+        int count = 0;
+        int number;
 
-        System.out.println("The random number is = " + shuffleNumber);
-        System.out.println("The number you've said is = " + number);
-    }
+        do{
+            System.out.println("Do you have to guess a secret number. So put a number equal or less than 50: ");
+            Scanner console = new Scanner(System.in);
+            number = Integer.parseInt(console.nextLine());
+            if(number > 50){
+                System.out.println("The number your're introduced is more than 50. Please introduce a number smaller.");
+            }
+            //System.out.println("The random number is = " + shuffleNumber);
+            System.out.println("The number you've said is = " + number);
+            count++;
+            System.out.println("count = " + count);
+            if(shuffleNumber > number){
+                System.out.println("The number your're introduced: " + number + " is less than the random number ");
+            }else if(shuffleNumber < number){
+                System.out.println("The number your're introduced: " + number + " is more than the random number");
+            }
+        }
+        while(shuffleNumber != number && count < 10);
+
+        if(shuffleNumber == number){
+            System.out.println("The number you're introduced is succeful: " + number + " cause the random number is: " + shuffleNumber);
+        }else{
+            System.exit(0);
+        }
+
+    };
 }
+
