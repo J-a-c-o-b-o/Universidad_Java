d1915eae46cdeb98bbfa2252d4e825575ad0d75c
j-a-c-o-b-o <correojacobo@gmail.com>
2025-01-24 18:20:42 +0100

    arrays

---
+++ src/Section9/Arrays.java
@@ -0,0 +1,14 @@
+package Section9;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] enteros;
+        enteros = new int[5];
+        enteros[4] = 4;
+        System.out.println("enteros posicion ultima= " + enteros[4]);
+        
+        int [] numeros = new int[4];
+        numeros[3] = 3;
+        System.out.println("numeros posicion ultima = " + numeros[3]);
+    }
+}
