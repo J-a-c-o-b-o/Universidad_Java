5ce6b7fde35b1a5b3dd0d4095998e1b03634d945
j-a-c-o-b-o <correojacobo@gmail.com>
2025-01-27 20:16:07 +0100

    arrays

--- src/Section9/Arrays.java
+++ src/Section9/Arrays.java
@@ -2,13 +2,20 @@ package Section9;
 
 public class Arrays {
     public static void main(String[] args) {
-        int[] enteros;
-        enteros = new int[5];
-        enteros[4] = 4;
-        System.out.println("enteros posicion ultima= " + enteros[4]);
+        int[] integers;
+        integers = new int[]{100, 200, 300, 400, 500};
+        integers[4] = 501;
+        System.out.println("last position= " + integers[4]);
         
         int [] numeros = new int[4];
         numeros[3] = 3;
-        System.out.println("numeros posicion ultima = " + numeros[3]);
+        System.out.println("third position = " + integers[3]);
+
+        for(var i = 0; i< integers.length; i++){
+            System.out.println("The content of the array is = " + i);
+        }
+
+        //promedio clasificaciones
+
     }
 }
