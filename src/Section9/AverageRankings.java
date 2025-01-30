3de9991c760e87ba9a21522f38d3415f6017ab5c
j-a-c-o-b-o <correojacobo@gmail.com>
2025-01-30 20:51:12 +0100

    updating code

--- src/Section9/AverageRankings.java
+++ src/Section9/AverageRankings.java
@@ -1,24 +1,57 @@
 package Section9;
 
+import java.util.Scanner;
+
 public class AverageRankings {
     public static void main(String[] args) {
-        int [] integers = new int[]{5, 6, 7, 3, 5, 6};
-        int sumOfIntegers = 0;
-        int totalSumOfIntegers = 0;
-        int average = 0;
+        Scanner console = new Scanner(System.in);
+        int sumOfGrades = 0;
+        int grade = 0;
+        String finish = "true";
+        String answerOfFinish = "false";
+        int[] grades;
 
-        for(var i = 0; i<integers.length; i++){
+
+        while(sumOfGrades < 10 || answerOfFinish == "false"){
+            System.out.println("Introduce the grade (max: 10 grades): ");
+            grade = Integer.parseInt(console.nextLine());
+                /*if (grade > 10) {
+                    System.out.println("The max grade is 10");
+                }*/
+            grades = new int[grade];
+            sumOfGrades += sumOfGrades;
+            System.out.println("Are you finish of introduce the grades? (true/false): ");
+            answerOfFinish = console.nextLine();
+            System.out.println("answerOfFinish = " + answerOfFinish);
+            if(answerOfFinish.equals(finish) || sumOfGrades >= 10){
+                console.close();
+            }
+
+        } ;
+
+            System.out.println("You are finished of introduced the grades");
+            console.close();
+
+
+
+            int sumOfIntegers = 0;
+            int totalSumOfIntegers = 0;
+            int average = 0;
+
+        /*for(var i = 0; i<grades.length; i++){
             System.out.println("\n*********** i in index: " + i + " **********");
-            System.out.println("Integer value = " + integers[i]);
+            System.out.println("Integer value = " + grades[i]);
             System.out.println("sumOfIntegers = " + sumOfIntegers);
-            sumOfIntegers += integers[i];
+            sumOfIntegers += grades[i];
             System.out.println("value integers " + sumOfIntegers + " is equal to = " + sumOfIntegers);
-        }
+        }*/
 
-        //Average
-        average = (sumOfIntegers / integers.length);
-        System.out.println("\n ***** The average of the grades is = " + average + " *****");
+            //Average
+        /*average = (sumOfIntegers / grades.length);
+        System.out.println("\n ***** The average of the grades is = " + average + " *****");*/
+
     }
-
-
 }
+
+
+
