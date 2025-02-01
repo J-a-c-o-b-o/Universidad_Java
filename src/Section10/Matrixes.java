a014a2c2da6afb22f22604769c9e8a13d7177bcc
j-a-c-o-b-o <correojacobo@gmail.com>
2025-02-01 15:01:37 +0100

    updating code

--- src/Section10/Matrixes.java
+++ src/Section10/Matrixes.java
@@ -19,7 +19,15 @@ public class Matrixes {
             for(var j=0; j<COLUMNS ;j++){
                 System.out.println("matrix[" + i + "][" + j +"] = " + matrix[i][j]);
             }
+        }
+        System.out.println("");
 
+        //Matrix2
+        var matrix2 = new int [][]{{111, 222, 333},{444, 555, 666}};
+        for(var i=0; i<matrix2.length ;i++){
+            for(var j=0; j<matrix2[i].length ;j++){
+                System.out.println("matrix2[" + i + "][" + j +"] = " + matrix2[i][j]);
+            }
         }
 
     }
