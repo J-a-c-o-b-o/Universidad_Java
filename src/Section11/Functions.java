99b80ab5d2d213d97f54db981a508198d4d0909c
j-a-c-o-b-o <correojacobo@gmail.com>
2025-02-03 13:03:06 +0100

    Functions

---
+++ src/Section11/Functions.java
@@ -0,0 +1,11 @@
+package Section11;
+
+public class Functions {
+
+    static void greet(String greet){
+        System.out.println("greet = " + greet);
+    }
+    public static void main(String[] args) {
+        greet("Hi");
+    }
+}
