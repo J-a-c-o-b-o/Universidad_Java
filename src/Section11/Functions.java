3f17075dcadf50b6d8c39b4e96918e66dd6e9dbc
J-a-c-o-b-o <correojacobo@gmail.com>
2025-02-05 20:51:05 +0100

    Functions

--- src/Section11/Functions.java
+++ src/Section11/Functions.java
@@ -1,7 +1,19 @@
 package Section11;
 
 public class Functions {
-    public static void main(String[] args) {
 
+    static void greet(String message){
+        System.out.println("message = " + message);
+    }
+
+    static int sum (int a, int b){
+        int result = a + b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        greet("Hi");
+        var resultInMain = sum(2, 4);
+        System.out.println("resultInMain = " + resultInMain);
     }
 }
