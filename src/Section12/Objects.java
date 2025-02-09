cc541ccd4c534aa9cbadfd02ee42f17bbc32b33a
j-a-c-o-b-o <correojacobo@gmail.com>
2025-02-09 11:49:45 +0100

    Person

--- src/Section12/Objects.java
+++ src/Section12/Objects.java
@@ -2,14 +2,16 @@ package Section12;
 
 public class Objects {
     public static void main(String[] args) {
+        //Instance of class Person
         var objectPerson = new Person();
+        //Attributes of the object of class Person
         objectPerson.name = "Jacobo";
         objectPerson.surname = "James";
-
+        //Method of the class Person through object
         objectPerson.showName();
-
+        //Method of the class Person through object (changing the attributes).
         objectPerson.addName("Michael", "Robinson");
-
+        //Method of the class Person through object
         objectPerson.showName();
     }
 }
