d14bd47ba9f1c91f60c40d35d1915f2336d64350
j-a-c-o-b-o <correojacobo@gmail.com>
2025-02-25 11:57:15 +0100

    inheritance

---
+++ src/Section12/Animal.java
@@ -0,0 +1,10 @@
+package Section12;
+
+public class Animal {
+    protected void eat(){
+        System.out.println("I eat a lot");
+    }
+    protected void sleep(){
+        System.out.println("I sleep a lot");
+    }
+}
---
+++ src/Section12/Dog.java
@@ -0,0 +1,7 @@
+package Section12;
+
+class Dog extends Animal {
+    public void makeSound(){
+        System.out.println("I can bark");
+    }
+}
