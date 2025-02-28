ca41d507ec40b648926a49197871643250509688
j-a-c-o-b-o <correojacobo@gmail.com>
2025-02-28 21:00:01 +0100

    inheritance2

---
+++ src/Section12/AnimalTest.java
@@ -0,0 +1,14 @@
+package Section12;
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        System.out.println("*** Hierarchy example ***");
+        System.out.println("Father class, I'm an animal");
+        var animal1 = new Animal();
+        animal1.eat();
+        animal1.sleep();
+        System.out.println("Child class, I'm a dog");
+        var dog1 = new Dog();
+
+    }
+}
