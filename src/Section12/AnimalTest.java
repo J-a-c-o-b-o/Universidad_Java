79726e4a546c6fce13bde473afbf61671fbbab43
j-a-c-o-b-o <correojacobo@gmail.com>
2025-03-14 09:39:14 +0100

    inheritance3

--- src/Section12/AnimalTest.java
+++ src/Section12/AnimalTest.java
@@ -5,10 +5,13 @@ public class AnimalTest {
         System.out.println("*** Hierarchy example ***");
         System.out.println("Father class, I'm an animal");
         var animal1 = new Animal();
-        animal1.eat();
-        animal1.sleep();
+        //animal1.eat();
+        //animal1.sleep();
         System.out.println("Child class, I'm a dog");
         var dog1 = new Dog();
+        dog1.eat();
+        dog1.sleep();
+        dog1.makeSound();
 
     }
 }
